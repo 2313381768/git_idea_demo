f957a0d142276bb7be355b8af5592cdccd371cf9
sizhou <2313381768@qq.com>
2021-09-29 20:05:25 +0800

    first commit

---
+++ src/main/java/com/jsu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.jsu.git;
+
+/**
+ * @title: GitTest
+ * @projectName: git_idea_demo
+ * @description: TODO
+ * @author: SiZhou
+ * @date: 2021/9/29 20:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
